0141db682c3cec008feba180d7f65662a75821db
Nastavni4kata <nastavni4kata@gmail.com>
2017-05-15 18:58:11 +0200

    sortiraj100broevi

--- nastavni4kata/sortiraj100broevi/src/com/viz/Main.java
+++ nastavni4kata/sortiraj100broevi/src/com/viz/Main.java
@@ -1,9 +1,38 @@
 package com.viz;
 
+import java.util.Random;
+
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("hi");
+
+		int[] nizaOdBroevi = new int[100];
+
+		Random r = new Random();
+		for (int i = 0; i < nizaOdBroevi.length; i++) {
+			nizaOdBroevi[i] = r.nextInt(5);
+		}
+
+		isprintajNiza(nizaOdBroevi);
+
+		for (int i = 0; i < nizaOdBroevi.length; i++) {
+			for (int j = 1; j < nizaOdBroevi.length-i; j++) {
+				if (nizaOdBroevi[j] < nizaOdBroevi[j-1]) {
+					int tmp = nizaOdBroevi[j-1];
+					nizaOdBroevi[j-1] = nizaOdBroevi[j];
+					nizaOdBroevi[j] = tmp;
+				}
+			}
+		}
+		
+		isprintajNiza(nizaOdBroevi);
+	}
+
+	public static void isprintajNiza(int[] niza) {
+		for (int i = 0; i < niza.length; i++) {
+			System.out.print(niza[i] + " ");
+		}
+		System.out.println();
 	}
 
 }
