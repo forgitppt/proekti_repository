4e9cefe565e283cbdcf94c70b14487214864cdce
Kris <kpecalevski@yahoo.com>
2017-05-17 19:56:21 +0200

    dvata sort od 10 i sort random

---
+++ Kris/randomNizaPodredi/src/com/proba/Main.java
@@ -0,0 +1,39 @@
+package com.proba;
+
+import java.util.Random;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int[] niza = new int[10];
+		for (int i = 0; i < niza.length; i++) {
+			niza[i] = (int) (Math.random() * 50 + 1);
+		}
+		for (int j = 0; j < niza.length; j++) {
+			System.out.print(" " + niza[j]);
+
+		}
+		System.out.println();
+
+		int[] nizaPodredi = niza;
+
+		int temp;
+		int min = 0;
+
+		for (int i = 0; i < nizaPodredi.length; i++) {
+			min = i;
+			 
+			for (int j = i; j < nizaPodredi.length; j++) {
+				if (nizaPodredi[j] < nizaPodredi[min])
+					min = j;
+			}
+			temp = nizaPodredi[i];
+			nizaPodredi[i] = nizaPodredi[min];
+			nizaPodredi[min] = temp;
+			System.out.print(" " + nizaPodredi[i]);
+
+		}
+	}
+}
