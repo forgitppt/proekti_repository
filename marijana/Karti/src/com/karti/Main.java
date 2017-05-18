1f713a3f0046e659518e1dffa4446d65318dadf7
Vizija12 <vizija12@gmail.com>
2017-05-18 21:32:35 +0200

    Karti pocetna verzija

---
+++ marijana/Karti/src/com/karti/Main.java
@@ -0,0 +1,19 @@
+package com.karti;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		Karta karta = new Karta("5ebb", "karo");
+		//Karta karta2 = new Karta("10", "detelina");
+
+		//karta.boja = "rozeva";
+		//karta.setBoja(Karta.SRCE);
+		
+		//karta.setBrojka(Karta.JACK);
+
+		System.out.println(karta);
+
+	}
+
+}
