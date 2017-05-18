6b4b637495cf5f6e9fc7eed8ba994c327c9e2a52
Kris <kpecalevski@yahoo.com>
2017-05-18 20:42:43 +0200

    proektKartioop

--- Kris/day3/src/com/proba/Main.java
+++ Kris/day3/src/com/proba/Main.java
@@ -3,8 +3,36 @@ package com.proba;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		int[] niza = { 3, 2, 6, 12, 8, 9 };
+		isprintaj(niza);
+		System.out.println();
+		
+		sort(niza);
+	}
+
+	public static void isprintaj(int[] nizaA) {
+		for (int i = 0; i < nizaA.length; i++) {
+			System.out.print(" " + nizaA[i]);
+		}
+	}
+
+	public static int[] sort(int[] sortNiza) {
+
+		int temp;
+		int min;
+		for (int i = 0; i < sortNiza.length; i++) {
+			min = i;
+			for (int j = i; j < sortNiza.length; j++) {
+				if (sortNiza[j] < sortNiza[min])
+					min = j;
+			}
+			temp = sortNiza[i];
+			sortNiza[i] = sortNiza[min];
+			sortNiza[min] = temp;
+			System.out.print(" " + sortNiza[i]);
+		}
+		return (sortNiza);
 	}
 
 }
