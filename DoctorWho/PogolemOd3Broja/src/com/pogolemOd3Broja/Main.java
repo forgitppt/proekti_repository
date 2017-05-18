453c7bc9add641284fe3b9672d105dd8a2e74e6e
Doctor Who <doctorwho@gmail.com>
2017-05-18 16:37:12 +0200

    pogolem od 3 broja 2 verzii

---
+++ DoctorWho/PogolemOd3Broja/src/com/pogolemOd3Broja/Main.java
@@ -0,0 +1,25 @@
+package com.pogolemOd3Broja;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int a = 12;
+		int b = 12;
+		int c = 1;
+		int d;
+		if (a >= b && a >= c) {
+			d = a;
+			System.out.println("brojot " + d + " e najgolem");
+		} else if (b >= a && b >= c) {
+			d = b;
+			System.out.println("brojot " + d + " e najgolem");
+		} else if (c >= a && c >= b) {
+			d = c;
+			System.out.println("brojot " + d + " e najgolem");
+
+		}
+
+	}
+}
