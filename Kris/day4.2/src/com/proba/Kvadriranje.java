f31e94a5fefe6a43b6831e7b3b2e95e475b20dbc
Kris <kpecalevski@yahoo.com>
2017-05-22 21:37:00 +0200

    day4programcinja

---
+++ Kris/day4.2/src/com/proba/Kvadriranje.java
@@ -0,0 +1,16 @@
+package com.proba;
+
+public class Kvadriranje {
+	
+	int broj;
+	
+	public void brojKvadriraj(int brojnaKvadrat){
+		brojnaKvadrat = broj*broj;
+		
+		System.out.println(brojnaKvadrat);
+		
+	}
+	
+	
+	
+}
---
+++ Kris/day4.2/src/com/proba/Vrakanje.java
@@ -0,0 +1,19 @@
+package com.proba;
+
+public class Vrakanje extends Kvadriranje{
+	
+	
+	int proveriBroj;
+	
+//	public static int finalBroj(int proveriBroj){
+//		if (brojnaKvadrat == brojnaKvadrat%2){
+//			proveriBroj = brojnaKvadrat;
+//		}
+//		else {
+//			proveriBroj = brojnaKvadrat * 2;
+//		}
+//		
+//		return proveriBroj;
+//	}
+
+}
