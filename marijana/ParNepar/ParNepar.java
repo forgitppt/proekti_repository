9370f698bf02630a7df64b0eb791c80d9244560b
Marijana <marijanastojkovska83@gmail.com>
2017-05-23 15:34:27 +0200

    parnineparni

---
+++ marijana/ParNepar/ParNepar.java
@@ -0,0 +1,22 @@
+package org.gitvezba;
+
+import java.util.Scanner;
+
+public class ParNepar {
+
+	 public static void main(String[] args) { 
+		 int num;
+		    System.out.println("Napisi broj");
+		    Scanner in = new Scanner(System.in);
+		    num = in.nextInt();
+		 
+		    if ( num % 2 == 0 )
+		      System.out.println("true");
+		    else
+		      System.out.println("false");
+		  }
+		   
+
+
+		  }
+	
