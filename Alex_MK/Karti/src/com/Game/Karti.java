ed1fde10b705af5f4338adbd918eab358d6f0b52
|AlexMK| <ablazevski@yahoo.com>
2017-05-30 22:48:11 +0200

    DALI E POTREBNA MATRICA

--- Alex_MK/Karti/src/com/Game/Karti.java
+++ Alex_MK/Karti/src/com/Game/Karti.java
@@ -14,5 +14,30 @@ public class Karti {
 	public static char QUEEN = 'Q';
 	public static char KING = 'K';
 	
-	
+	// spil na karti 
+//	public static void spil();
+//	String[] spil = new String[53];
+//	int brojNaKarti = 0;
+//	for (int i = 1; i < 5; i++) {
+//		if (i == 1) {
+//			kartata = Karti.BAKLAVA;
+//		}
+//		if (i == 2) {
+//			kartata = Karti.SRCE;
+//		}
+//		if (i == 3) {
+//			kartata = Karti.DETELINA;
+//		}
+//		if (i == 0) {
+//			kartata = Karti.LIST;
+//		}
+//
+//		for (int j = 1; j < 14; j++) {
+//			brojNaKarti++;
+//			spil[brojNaKarti] = (j + " " + kartata);
+//			System.out.println(brojNaKarti + "---> " + spil[brojNaKarti]);
+//		}
+//	}
+//
+//	System.out.println(spil[45]);
 	}
