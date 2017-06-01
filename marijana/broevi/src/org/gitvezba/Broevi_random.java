8d650bb99c995acfeb91af69911243442f455545
Marijana <marijanastojkovska83@gmail.com>
2017-06-01 12:20:05 +0200

    pomos okolu zadacava ;))

--- marijana/broevi/src/org/gitvezba/Broevi_random.java
+++ marijana/broevi/src/org/gitvezba/Broevi_random.java
@@ -9,12 +9,13 @@ public class Broevi_random {
 
 		int[] numbers = new int[15];
 		int i = 0;
+		
+		
 		Random numbers1 = new Random();
 		for (i = 0; i < numbers.length; i++) {
 			numbers[i] = numbers1.nextInt(10);
-
-
-			System.out.print(numbers[i] + " ");
+System.out.print(numbers[i] + " ");
+             
 			
 			
 		}
